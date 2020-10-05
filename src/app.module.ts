ebd31bc2d6f95ea507eaaa1ad9d77e651081bcb5
Dominik Ostrowski <dominik.ostrowski@live.com>
2020-10-05 07:57:07 +0200

    feat(db): add sqlite3 db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: ":memory:",
+      dropSchema: true,
+      entities: [],
+      synchronize: true,
+      logging: false
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
